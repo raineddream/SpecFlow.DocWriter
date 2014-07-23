ff889f45131dd68ffadb0e345f8b63c0e0dfdad7
raineddream <raineddream@gmail.com>
2014-07-23 09:48:11 +0800

    Read in scenario description

---
+++ DocWriterTest/Reader/ScenarioReaderTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Rain.SpecFlow.DocWriter.Reader
+{
+    [TestFixture]
+    public class ScenarioReaderTest
+    {
+        [Test]
+        public void Should_read_in_scenario_description()
+        {
+            string firstLine = "Scenario: The answer with the highest vote gets to the top";
+            var reader = new ScenarioReader(firstLine, BufferedReaderBuilder.ReadIn(firstLine));
+
+            Scenario scenario = reader.Read();
+
+            Assert.That(scenario.Description, Is.EqualTo("The answer with the highest vote gets to the top"));
+        }
+    }
+}
